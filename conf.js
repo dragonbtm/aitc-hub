d84b78e35e20056c65b157e4a4e8f2f031f60452
zz <zz634682577@163.com>
2019-06-21 16:10:21 +0800

    init 创世

--- conf.js
+++ conf.js
@@ -12,36 +12,39 @@ exports.KEYS_FILENAME = 'keys.json';
 exports.bLight = false;
 exports.bServeAsHub = true;
 
-exports.port = 10060;
+exports.port = 8286;
 exports.rpcPort = '10061';
 
 
 exports.initial_witnesses = [
-	'O4JNHW52CF5BDGASQEVLAVZGBMNHYZ2S',
-	'QLKAIB33R7AVHABYC5PGER2LLIVH5YE2',
-	'IOOOMPPD6FRHTNQBK763EAFUCIRJZYMP'
+    'WXNM4L4OTGIKSQHMMMBE6TUW6YJ4SHGY',
+    'J4LFUWCFCXLWNNCLGMVGQKXUOUULNPDW',
+    'DLVXBKXVNSOLGSXVAJBUYQUG5OEGWTV5',
+    'JNR2E5BPXI576ZEVR5S5HVTYC6EZNDPW',
+    'SLNK5ISPUQRJX2VIMUTU7YHMNTGOWUEC',
+    'SDO4KLWIPLLKBWMW5FJSWBCWBHHOC2OI'
 ];
 
 
-exports.admin_email='zz634682577@163.com';
-exports.from_email='wenzhengzhang@chenhaninfo.com';
-exports.smtpTransport ="relay";
-exports.smtpUser='wenzhengzhang@chenhaninfo.com';
-exports.smtpPassword='Aa123123';
-exports.smtpRelay='smtp.mxhichina.com';
-exports.smtpPort='25';
+// exports.admin_email='zz634682577@163.com';
+// exports.from_email='wenzhengzhang@chenhaninfo.com';
+// exports.smtpTransport ="relay";
+// exports.smtpUser='wenzhengzhang@chenhaninfo.com';
+// exports.smtpPassword='Aa123123';
+// exports.smtpRelay='smtp.mxhichina.com';
+// exports.smtpPort='25';
 
 
 
 exports.initial_peers = [
-    'wss://hub1.ebaoedu.com:10060',
-    'wss://hub2.ebaoedu.com:10060',
-    'wss://hub3.ebaoedu.com:10060',
-    'wss://hub4.ebaoedu.com:10060'
+    'ws://172.31.34.124:8286',
+    'ws://172.31.34.120:8286',
+    'ws://172.31.34.125:8286',
+    'ws://172.31.34.118:8286'
 ];
 
 exports.trustedRegistries = {
-	'O4JNHW52CF5BDGASQEVLAVZGBMNHYZ2S': 'market'
+	// 'O4JNHW52CF5BDGASQEVLAVZGBMNHYZ2S': 'market'
 };
 
 console.log('finished hub conf');
